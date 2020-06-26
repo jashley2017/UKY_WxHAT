19c1e4711dd22c320d25b64567748ee26e33632c
jashley2017 <jashley2017@gmail.com>
2020-06-26 17:58:24 -0400

    added calibration to bno080 python library

--- bno080_i2c.py
+++ bno080_i2c.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 from smbus2 import SMBus, i2c_msg
 import time
+import sys
 import board
 import digitalio
+
+CURSOR_UP_ONE = '\x1b[1A'
+ERASE_LINE = '\x1b[2K'
+def delete_last_lines(n=1):
+    for _ in range(n):
+        sys.stdout.write(CURSOR_UP_ONE)
+        sys.stdout.write(ERASE_LINE)
 
 class BNO080_I2C:
     
@@ -76,16 +84,52 @@ class BNO080_I2C:
         self.addr = addr
         self.bus = SMBus(port)
         self.sequence = [0]*5
+        self.get_ad()
 
     @classmethod
     def _i2c_delay(cls):
         time.sleep(cls.I2CDELAY/1000000)
 
-    def start_rot(self):
-        pass
-
     def start_calib(self):
-        pass
+        me_calib = [
+                self.COMMAND_REQUEST, 
+                self.sequence[self.CHANNEL_CONTROL], 
+                0x07,
+                0x01,
+                0x01,
+                0x01,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00
+        ]
+        self.send_shtp(self.CHANNEL_CONTROL, me_calib)
+
+        res_head, res_data = self.receive_shtp_until(self.CHANNEL_CONTROL, [self.COMMAND_RESPONSE])
+        count = 0
+        while res_data[2] != 0x07 and count < 3:
+            res_head, res_data = self.receive_shtp_until(self.CHANNEL_COMMAND, [self.COMMAND_RESPONSE])
+            count += 1
+        if res_data[2] == 0x07 and res_data[5] != 0:
+            print(f"ERROR: ME calibration set command failed: {res_data}")
+        
+        self.start_game_rot(10)
+        self.start_mag(50)
+
+        report_dict = self.get_report([self.SENSOR_REPORTID_MAG])
+        mag_rep = report_dict.get(self.SENSOR_REPORTID_MAG, [[0x00, 0x00, 0x00]])[-1] # most recent mag report
+
+        while mag_rep[2] & 0x03 != 0x03:
+            print(mag_rep[2] & 0x03)
+            time.sleep(1/50)
+            delete_last_lines(1)
+            report_dict = self.get_report([self.SENSOR_REPORTID_MAG])
+            mag_rep = report_dict.get(self.SENSOR_REPORTID_MAG, [[0x00, 0x00, 0x00]])[-1] # most recent mag report
+
+        print("Calibration Success!")
+        return True
 
     def set_feature(self, sensor, features, sensitivity, rate, batch, sensor_spec):
         feat_cmd = [
@@ -114,11 +158,11 @@ class BNO080_I2C:
         res_head, res_data = self.receive_shtp_until(self.CHANNEL_CONTROL, [self.GET_FEATURE_RESPONSE])
 
         # TODO: Log this on debug and check the values
-        print(f"Sensor: {res_data[1]}");
-        print(f"Feature Flags: {res_data[2]}"); 
-        print(f"Sensitivity byte: {res_data[4]}, {res_data[3]}"); 
-        print(f"Report Interval word:  {res_data[8]} {res_data[7]} {res_data[6]} {res_data[5]}"); 
-        print(f"Sensor Specific Config word: {res_data[13]}, {res_data[14]}, {res_data[15]}, {res_data[16]}");
+        # print(f"Sensor: {res_data[1]}");
+        # print(f"Feature Flags: {res_data[2]}"); 
+        # print(f"Sensitivity byte: {res_data[4]}, {res_data[3]}"); 
+        # print(f"Report Interval word:  {res_data[8]} {res_data[7]} {res_data[6]} {res_data[5]}"); 
+        # print(f"Sensor Specific Config word: {res_data[13]}, {res_data[14]}, {res_data[15]}, {res_data[16]}");
 
         return True
 
@@ -127,6 +171,12 @@ class BNO080_I2C:
 
     def start_rot(self, rate):
         return self.set_feature(self.SENSOR_REPORTID_ROT, 0, 0, rate, 0, 0)
+
+    def start_game_rot(self, rate):
+        return self.set_feature(self.SENSOR_REPORTID_GAM, 0, 0, rate, 0, 0)
+
+    def start_mag(self, rate):
+        return self.set_feature(self.SENSOR_REPORTID_MAG, 0, 0, rate, 0, 0)
 
     def get_report(self, report_types): 
         report_types.append(self.GET_TIME_REFERENCE)
@@ -149,13 +199,12 @@ class BNO080_I2C:
     def get_shtp_errors(self):
         self.send_shtp(self.CHANNEL_COMMAND, [0x01])
         res_head, res_data = self.receive_shtp_until(self.CHANNEL_COMMAND, [0x01], 5)
-        # TODO: i2c_bno080.c has the error list
-        return res_data[1:]
 
     def get_prod_inf(self):
         self.send_shtp(self.CHANNEL_CONTROL, [self.PRODUCT_ID_REQUEST])
-        res_head, res_data = self.receive_shtp_until(self.CHANNEL_CONTROL, [self.PRODUCT_ID_RESPONSE])
-        return res_data
+        _res_head1, res_data1 = self.receive_shtp_until(self.CHANNEL_CONTROL, [self.PRODUCT_ID_RESPONSE])
+        _res_head2, res_data2 = self.receive_shtp_until(self.CHANNEL_CONTROL, [self.PRODUCT_ID_RESPONSE])
+        return [res_data1, res_data2]
 
     def send_shtp(self, channel, data):
         '''
@@ -211,7 +260,10 @@ class BNO080_I2C:
         
         if len(msg) != packetlen or len(msg) == 0:
             # print ("Packet Error")
-            return [None, None]
+            return [[0]*4, [0]*4]
+
+        if msg[2] == 0 and msg[0+4] == 0x01:
+            self.print_shtp_errs(msg[4:])
 
         self.sequence[msg[2]] = msg[3] # sync the sequence with the IMU
 
@@ -219,9 +271,99 @@ class BNO080_I2C:
         # print(print_hex(msg))
 
         return [msg[0:4], msg[4:]]
+    
+    @staticmethod
+    def print_shtp_errs(err_data):
+        if len(err_data) == 1:
+            return None
 
-def print_hex(hexs):
-    return [hex(h) for h in hexs]
+        err_dict = [
+            "No Error",
+            "Hub application attempted to exceed maximum read cargo length",
+            "Hub application attempted to exceed maximum read cargo length",
+            "Host wrote a header with length greater than maximum write cargo length",
+            "Host wrote a header with length <= header length (invalid or no payload)",
+            "Host tried to fragment cargo (transfer length < full cargo length)",
+            "Host wrote continuation of fragmented cargo (continuation bit set)",
+            "Unrecognized command on control channel (2)",
+            "Unrecognized parameter to get-advertisement command",
+            "Host wrote to unrecognized channel",
+            "Advertisement request received while Advertisement Response was pending",
+            "Host write before the hub finished sending advertisement response",
+            "Error list too long to send, truncated"
+        ]
+                
+        print("ERROR: the following errors were reported from the IMU:")
+        for err in err_data[1:]:
+            print(f"\t{err_dict[err]}")
+    
+    def get_ad(self):
+        res_head, res_data = self.receive_shtp()
+        if res_head[2] == self.CHANNEL_COMMAND and res_data[0] == 0x00:
+            # TODO: log not print, and get something out of this ad
+            print("Got advertisement")
+        else:
+            print("Did not get advertisement, moving on")
+
+    @classmethod
+    def print_rep(cls, rep):
+        rep_methods = {
+                cls.SENSOR_REPORTID_ACC: cls._print_acc, 
+                cls.SENSOR_REPORTID_GYR: cls._print_gyr,
+                cls.SENSOR_REPORTID_MAG: cls._print_mag,
+                cls.SENSOR_REPORTID_LIN: cls._print_lin,
+                cls.SENSOR_REPORTID_ROT: cls._print_rot,
+                cls.SENSOR_REPORTID_GRA: cls._print_gra,
+                cls.SENSOR_REPORTID_GAM: cls._print_gam,
+                cls.SENSOR_REPORTID_GEO: cls._print_geo,
+                cls.SENSOR_REPORTID_TAP: cls._print_tap,
+                cls.SENSOR_REPORTID_STP: cls._print_stp,
+                cls.SENSOR_REPORTID_STA: cls._print_sta,
+                cls.SENSOR_REPORTID_PER: cls._print_per
+        }
+        rep_methods[rep[0]](rep)
+
+    @classmethod
+    def _print_acc(cls, rep): 
+        print(f" Acceleration: stat: {hex(rep[2])} delay: {hex(rep[3])} x: {cls.print_hex(rep[5:3:-1])} y: {cls.print_hex(rep[7:5:-1])} z: {cls.print_hex(rep[9:7:-1])}")
+    @classmethod
+    def _print_gyr(cls, rep):
+        pass
+    @classmethod
+    def _print_mag(cls,rep):
+        pass
+    @classmethod
+    def _print_lin(cls,rep):
+        pass
+    @classmethod
+    def _print_rot(cls,rep):
+        print(f" Rotation: stat: {hex(rep[2])} delay: {hex(rep[3])} i: {cls.print_hex(rep[5:3:-1])} j: {cls.print_hex(rep[7:5:-1])} k: {cls.print_hex(rep[9:7:-1])} r: {cls.print_hex(rep[11:9:-1])} accuracy: {cls.print_hex(rep[13:11:-1])} ")
+    @classmethod
+    def _print_gra(cls,rep):
+        pass
+    @classmethod
+    def _print_gam(cls,rep):
+        pass
+    @classmethod
+    def _print_geo(cls,rep):
+        pass
+    @classmethod
+    def _print_tap(cls,rep):
+        pass
+    @classmethod
+    def _print_stp(cls,rep):
+        pass
+    @classmethod
+    def _print_sta(cls,rep):
+        pass
+    @classmethod
+    def _print_per(cls,rep):
+        pass
+    @classmethod
+    def print_hex(cls,hexs):
+        return '0x' + ''.join([hex(h).strip('0x').zfill(2) for h in hexs])
+
+
 
 if __name__ == '__main__':
     # reset to start with a clean buffer
@@ -231,17 +373,26 @@ if __name__ == '__main__':
     time.sleep(1)
     rst.value = True
     time.sleep(1)
-
+    
     imu = BNO080_I2C(0x4a, 3)
-    errs = imu.get_shtp_errors()
-    if len(errs) > 0: 
-        print(f"WARNING: IMU has {len(errs)} errors: {print_hex(errs)}.")
+    imu.get_shtp_errors()
     prod_inf = imu.get_prod_inf()
-    print(f"Product info: {print_hex(prod_inf)}")
+    print(f"Product info: {imu.print_hex(prod_inf[0])}\n{imu.print_hex(prod_inf[1])}")
 
-    imu.start_rot(10)
-    imu.start_acc(10)
+    # imu.start_calib()
 
-    time.sleep(0.2)
-    rep = imu.get_report([imu.SENSOR_REPORTID_ACC, imu.SENSOR_REPORTID_ROT])
-    print(rep)
+    imu.start_rot(20)
+    imu.start_acc(20)
+
+    time.sleep(1)
+    while True:
+        reps = imu.get_report([imu.SENSOR_REPORTID_ACC, imu.SENSOR_REPORTID_ROT])
+        acc_rep = reps.get(imu.SENSOR_REPORTID_ACC, [False])[-1]
+        rot_rep = reps.get(imu.SENSOR_REPORTID_ROT, [False])[-1]
+        count = 0 
+        if acc_rep and rot_rep:
+            imu.print_rep(acc_rep)
+            imu.print_rep(rot_rep)
+            count = 2 
+        time.sleep(0.5)
+        delete_last_lines(count)
